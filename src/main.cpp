97c21239f376c92cdbe14f6b1fff2895dc92dfd2
cocoa <95043086+hotcocoaNcode@users.noreply.github.com>
2025-04-22 22:06:27 -0500

    add test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
+#include <iomanip>
 
-#define _EXTALLOC_DEBUGS_ENABLED
-#define _EXTALLOC_BLOCKS_PER_ARR 256
+//#define _EXTALLOC_DEBUGS_ENABLED
+#define _EXTALLOC_BLOCKS_PER_ARR 16
 #define _EXTALLOC_CHUNK_SIZE 128
 
 extern "C" uint8_t external_psram_size;
@@ -123,7 +124,7 @@ namespace extalloc {
 		return malloc(size);
 	}
 
-	void free(void* ptr) {
+	void free(volatile void* ptr) {
 		if (memory_enable) {
 			const auto index = static_cast<block_int_t>((*reinterpret_cast<uint32_t*>(&ptr)-0x70000000)/128);
 			block b{};
@@ -166,15 +167,84 @@ namespace extalloc {
 				check_for_next_block(chunk, i);
 			}
 		} else {
-			::free(ptr);
+			::free(const_cast<void*>(ptr));
 		}
 	}
+}
+
+void fill(volatile void* addr, const uint8_t value, const size_t size) {
+	for (size_t i = 0; i < size; i++) {
+		static_cast<volatile uint8_t*>(addr)[i] = value;
+	}
+}
+
+void check(volatile void* addr, const uint8_t value, const size_t size) {
+	for (size_t i = 0; i < size; i++) {
+		if (static_cast<volatile uint8_t*>(addr)[i] != value) {
+			Serial.printf("Check for %x failed at %llu : %llu (size %u)", value, addr, i, size);
+		}
+	}
+	Serial.println("Check finished");
 }
 
 void setup()
 {
 	while (!Serial) ; // wait
 	extalloc::init();
+	ARM_DEMCR |= ARM_DEMCR_TRCENA;
+	ARM_DWT_CTRL |= ARM_DWT_CTRL_CYCCNTENA;
+
+	volatile void *a = extalloc::alloc(256);
+	volatile void *b = extalloc::alloc(256);
+	volatile void *c = extalloc::alloc(256);
+	volatile void *d = extalloc::alloc(256);
+
+	fill(a, 0, 256);
+	fill(b, 1, 256);
+	fill(c, 2, 256);
+	fill(d, 3, 256);
+
+	check(a, 0, 256);
+	check(b, 1, 256);
+	check(c, 2, 256);
+	check(d, 3, 256);
+
+	Serial.printf("a %08X, b %08X, c %08X, d %08X\n", a, b, c, d);
+
+	extalloc::free(c);
+
+
+
+	uint32_t c_start = ARM_DWT_CYCCNT;
+	volatile void *e = extalloc::alloc(128);
+	uint32_t c_end = ARM_DWT_CYCCNT;
+	Serial.printf("e's alloc took %lu cycles\n", c_end-c_start);
+	c_start = ARM_DWT_CYCCNT;
+	volatile void *f = extalloc::alloc(128);
+	c_end = ARM_DWT_CYCCNT;
+	Serial.printf("f's alloc took %lu cycles\n", c_end-c_start);
+
+	Serial.printf("e %08X, f %08X\n", e, f);
+
+
+	fill(e, 4, 128);
+	fill(f, 5, 128);
+
+	check(e, 4, 128);
+	check(f, 5, 128);
+
+	c_start = ARM_DWT_CYCCNT;
+	extalloc::free(a);
+	c_end = ARM_DWT_CYCCNT;
+	Serial.printf("a's free took %lu cycles\n", c_end-c_start);
+
+	c_start = ARM_DWT_CYCCNT;
+	extalloc::free(b);
+	extalloc::free(d);
+	extalloc::free(e);
+	extalloc::free(f);
+	c_end = ARM_DWT_CYCCNT;
+	Serial.printf("the rest took %lu cycles\n", c_end-c_start);
 }
 
 
